fb07ee871b489ea2bd4145068a5e7c040b682808
Yuqi Zhou <yuqzho@microsoft.com>
2022-07-24 22:30:49 +0800

    chore: m365 draft

--- packages/fx-core/src/component/fx/preProvisionAction.ts
+++ packages/fx-core/src/component/fx/preProvisionAction.ts
@@ -12,18 +12,24 @@ import {
   ProvisionContextV3,
   Result,
   UserError,
+  v2,
+  v3,
+  Void,
 } from "@microsoft/teamsfx-api";
 import { getLocalizedString } from "../../common/localizeUtils";
 import { hasAzureResourceV3 } from "../../common/projectSettingsHelperV3";
 import { globalVars } from "../../core";
+import { SolutionSource } from "../../plugins";
 import { updateResourceBaseName } from "../../plugins/solution/fx-solution/arm";
 import { resourceGroupHelper } from "../../plugins/solution/fx-solution/utils/ResourceGroupHelper";
+import { BuiltInFeaturePluginNames } from "../../plugins/solution/fx-solution/v3/constants";
 import {
   askForProvisionConsent,
   fillInAzureConfigs,
   getM365TenantId,
 } from "../../plugins/solution/fx-solution/v3/provision";
 import { ComponentNames } from "../constants";
+import fs from "fs-extra";
 
 export class FxPreProvisionAction implements FunctionAction {
   name = "fx.preProvision";
@@ -47,19 +53,22 @@ export class FxPreProvisionAction implements FunctionAction {
     }
     const tenantIdInToken = tenantIdInTokenRes.value;
     if (tenantIdInConfig && tenantIdInToken && tenantIdInToken !== tenantIdInConfig) {
-      return err(
-        new UserError(
-          "Solution",
-          "TeamsAppTenantIdNotRight",
-          getLocalizedString("error.M365AccountNotMatch", envInfo.envName)
-        )
+      const checkM365TenantRes = await checkM365Tenant(
+        ctx,
+        envInfo,
+        tenantIdInToken,
+        tenantIdInConfig,
+        inputs.projectPath
       );
+      if (checkM365TenantRes.isErr()) {
+        return err(checkM365TenantRes.error);
+      }
     }
-    if (!tenantIdInConfig) {
-      appManifest.tenantId = tenantIdInToken;
-      solutionConfig.teamsAppTenantId = tenantIdInToken;
-      globalVars.m365TenantId = tenantIdInToken;
-    }
+
+    appManifest.tenantId = tenantIdInToken;
+    solutionConfig.teamsAppTenantId = tenantIdInToken;
+    globalVars.m365TenantId = tenantIdInToken;
+
     // 3. check Azure configs
     if (hasAzureResourceV3(ctx.projectSetting) && envInfo.envName !== "local") {
       // ask common question and fill in solution config
@@ -99,4 +108,93 @@ export class FxPreProvisionAction implements FunctionAction {
     }
     return ok([]);
   }
+}
+
+export async function checkM365Tenant(
+  ctx: v2.Context,
+  envInfo: v3.EnvInfoV3,
+  tenantIdInToken: string,
+  tenantIdInConfig: string,
+  projectPath: string | undefined
+): Promise<Result<checkM365TenantResult, FxError>> {
+  const hasSwitchedM365Tenat =
+    !!tenantIdInConfig && !!tenantIdInToken && tenantIdInToken !== tenantIdInConfig;
+  const localFiles = [];
+  if (hasSwitchedM365Tenat) {
+    if (envInfo.envName === "local") {
+      localFiles.push(".fx/states/state.local.json");
+      // add notification local file if exist
+      if (
+        projectPath !== undefined &&
+        (await fs.pathExists(`${projectPath}/bot/.notification.localstore.json`))
+      ) {
+        localFiles.push("bot/.notification.localstore.json");
+      }
+    }
+    const confirmResult = await askForM365TenantConfirm(
+      ctx,
+      envInfo,
+      tenantIdInConfig,
+      tenantIdInToken,
+      localFiles
+    );
+    if (confirmResult.isErr()) {
+      return err(confirmResult.error);
+    } else {
+      if (envInfo.envName !== "local") {
+        const keysToClear = [
+          BuiltInFeaturePluginNames.bot,
+          BuiltInFeaturePluginNames.aad,
+          ComponentNames.TeamsBot,
+          ComponentNames.AadApp,
+        ];
+        const keys = Object.keys(envInfo.state);
+        for (let index = 0; index < keys.length; index++) {
+          if (keysToClear.includes(keys[index])) {
+            envInfo.state[keys[index]] = {};
+          }
+        }
+
+        // todo: update bot resource base name. see: https://github.com/OfficeDev/TeamsFx/compare/dev...yuqzho/switch-m365?expand=1
+      } else {
+        const keys = Object.keys(envInfo.state);
+        for (let index = 0; index < keys.length; index++) {
+          envInfo.state[keys[index]] = {};
+        }
+        // todo: delete local files.
+      }
+    }
+
+    return ok({ hasSwitchedM365Tenant: true });
+  }
+
+  return ok({ hasSwitchedM365Tenant: false });
+}
+
+async function askForM365TenantConfirm(
+  ctx: v2.Context,
+  envInfo: v3.EnvInfoV3,
+  localDebugTenantId: string,
+  maybeM365TenantId: string,
+  localFiles: string[]
+): Promise<Result<Void, FxError>> {
+  const msg = getLocalizedString(
+    // ask for unmatched tenant
+    "core.localDebug.tenantConfirmNotice",
+    localDebugTenantId,
+    maybeM365TenantId,
+    !localFiles ? "" : localFiles.join(", ")
+  );
+  const confirmRes = await ctx.userInteraction.showMessage("warn", msg, true, "Continue");
+  const confirm = confirmRes?.isOk() ? confirmRes.value : undefined;
+
+  if (confirm !== "Continue") {
+    return err(new UserError(SolutionSource, "CancelLocalDebug", "CancelLocalDebug"));
+  }
+
+  return ok();
+}
+
+export interface checkM365TenantResult {
+  hasSwitchedM365Tenant: boolean;
 }
